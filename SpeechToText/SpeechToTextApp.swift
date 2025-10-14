a7563a197d8e123e6872c6402cb2f4ec32f48995
Azmal Awasaf <aawsaf@mymedicalhub.com>
2025-10-13 23:01:55 -0600

    Initial Commit

---
+++ SpeechToText/SpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextApp.swift
+//  SpeechToText
+//
+//  Created by Azmal Awsaf on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeechToTextTests/SpeechToTextTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextTests.swift
+//  SpeechToTextTests
+//
+//  Created by Azmal Awsaf on 13/10/25.
+//
+
+import Testing
+@testable import SpeechToText
+
+struct SpeechToTextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
